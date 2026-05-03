d7537e6a3d6dd40fe2b4cfca7d8cc1164c8e7e01
Reece-Lu <53824755+Reece-Lu@users.noreply.github.com>
2026-05-02 17:33:58 -0700

    Initial Commit

---
+++ RunInsight/RunInsightApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunInsightApp.swift
+//  RunInsight
+//
+//  Created by Yuwen on 2026-05-02.
+//
+
+import SwiftUI
+
+@main
+struct RunInsightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RunInsightTests/RunInsightTests.swift
@@ -0,0 +1,17 @@
+//
+//  RunInsightTests.swift
+//  RunInsightTests
+//
+//  Created by Yuwen on 2026-05-02.
+//
+
+import Testing
+@testable import RunInsight
+
+struct RunInsightTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
